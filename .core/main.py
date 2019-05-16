e718549f306ffbd5bf904f42c460a6b9b8bf8874
rexsllemel <rexs.llemel333@gmail.com>
2019-05-16 10:07:35 +0800

    Update main.py

--- .core/main.py
+++ .core/main.py
@@ -96,9 +96,7 @@ try:
   UravxBuCwNMpYWTzKhPI("\033[1;31m██╔══██╗██║     ██╔══██║\033[1;37m██╔══██╗██║   ██║   ██║   ")
   UravxBuCwNMpYWTzKhPI("\033[1;31m██████╔╝╚██████╗██║  ██║\033[1;37m██████╔╝╚██████╔╝   ██║   ")
   UravxBuCwNMpYWTzKhPI("\033[1;31m╚═════╝  ╚═════╝╚═╝  ╚═╝\033[1;37m╚═════╝  ╚═════╝    ╚═╝  \n")
-  UravxBuCwNMpYWTzKhPI(" \033[1;96m[\033[0;37m+\033[1;96m] \033[0;36mAuthor   \033[0;37m: Njank Soekamti")
-  UravxBuCwNMpYWTzKhPI(" \033[1;96m[\033[0;37m+\033[1;96m] \033[0;36mYoutube  \033[0;37m: Njank Soekamti")
-  UravxBuCwNMpYWTzKhPI(" \033[1;96m[\033[0;37m+\033[1;96m] \033[0;36mGithub   \033[0;37m: N74NK\n")
+  UravxBuCwNMpYWTzKhPI(" \033[1;96m[\033[0;37m+\033[1;96m] \033[0;36mModded by   \033[0;37m: PiLoY")
  async def UravxBuCwNMpYWTzKhPF():
   if not UravxBuCwNMpYWTzKhPb.exists("session"):
    UravxBuCwNMpYWTzKhPo("session")
